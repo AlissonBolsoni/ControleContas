4236e80aa526ff1c58480dee76df47aa3dc0c023
alisson <alisson.bolsoni@gmail.com>
2019-02-05 08:09:18 -0200

    redução do tempo de espera da thread

--- app/src/main/java/br/com/alisson/billcontrol/services/CheckAlarm.kt
+++ app/src/main/java/br/com/alisson/billcontrol/services/CheckAlarm.kt
@@ -58,7 +58,7 @@ class CheckAlarm(private val context: Context, private val alarmService: AlarmSe
                     alarmService.createAlarm(bills)
                 }
             }
-            Thread.sleep(TimeUnit.HOURS.toMillis(8))
+            Thread.sleep(TimeUnit.HOURS.toMillis(5))
         }
         this.status = STOP
     }
